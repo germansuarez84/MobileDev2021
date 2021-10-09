c2b918e1e4ae3f18597d526e10109a5721542b34
German Suárez <germancloud@gmail.com>
2021-10-09 07:00:14 -0500

    Versión 1.0 Calculadora

--- calculadora/src/main/java/com/example/calculadora/MainActivity.java
+++ calculadora/src/main/java/com/example/calculadora/MainActivity.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+import java.util.*;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -65,13 +66,17 @@ public class MainActivity extends AppCompatActivity {
         btnMul= findViewById(R.id.btnMul);
         btnDiv= findViewById(R.id.btnDiv);
         btnEq=  findViewById(R.id.btnEqual);
+        btnAC= findViewById(R.id.btnAC);
 
         btnPlus.setOnClickListener(operatorHandler);
         btnMin.setOnClickListener(operatorHandler);
         btnMul.setOnClickListener(operatorHandler);
         btnDiv.setOnClickListener(operatorHandler);
+        btnEq.setOnClickListener(operatorHandler);
+        btnAC.setOnClickListener(AcHandler);
 
         btnComma= findViewById(R.id.btnComma);
+        btnComma.setOnClickListener(commaHandler);
 
 
     }
@@ -92,6 +97,7 @@ public class MainActivity extends AppCompatActivity {
         etResultCalc= findViewById(R.id.etResultCalc);
 
        // Toast.makeText(this, "resultText="+ resultText, Toast.LENGTH_LONG).show();
+
         if(resultText.compareTo("0")==0)
             resultText=strNumber;
         else
@@ -113,6 +119,7 @@ public class MainActivity extends AppCompatActivity {
                 case R.id.btnMin:addOperator(2);break;
                 case R.id.btnMul:addOperator(3);break;
                 case R.id.btnDiv:addOperator(4);break;
+                case R.id.btnEqual:addOperator(5);break;
             }
 
         }
@@ -121,28 +128,60 @@ public class MainActivity extends AppCompatActivity {
 
 
         etResultCalc= findViewById(R.id.etResultCalc);
-        //Toast.makeText(this, "existFirstFactor="+ String.valueOf(existFirstFactor), Toast.LENGTH_LONG).show();
+        //Toast.makeText(this, "operador="+ String.valueOf(op), Toast.LENGTH_SHORT).show();
+        //Formato doble
+
             if(!existFirstFactor)
-            {firstFactor= Double.valueOf(etResultCalc.getText().toString());
+            {firstFactor= Double.valueOf(getDoubleFormat(etResultCalc.getText().toString()));
                 operator=op;
                 resultText="0";
                 isChanged=false;
                 existFirstFactor=true;
             }
             else
-            { if(isChanged)
-                {   //Ya había un factor
-                    //Haga el cálculo con el operador que había
-                    firstFactor= calculate(Double.valueOf(etResultCalc.getText().toString()));
-                    etResultCalc.setText(getStringFormat(firstFactor));
+            { if(isChanged) {   //Ya había un factor
+                //Haga el cálculo con el operador que había
+                firstFactor = calculate(Double.valueOf(getDoubleFormat(etResultCalc.getText().toString())));
+                etResultCalc.setText(getStringFormat(firstFactor));
+
+            }
                     resultText="0";
                     isChanged=false;
+                    operator=op;
                 }
+            if(op==5)
+                operator=0;
 
             }
 
+    View.OnClickListener AcHandler= new View.OnClickListener(){
+        @Override
+        public void onClick(View view) {
+            etResultCalc= findViewById(R.id.etResultCalc);
+            etResultCalc.setText("0");
+        }
+    };
 
-    }
+    View.OnClickListener commaHandler= new View.OnClickListener(){
+        @Override
+        public void onClick(View view) {
+            addComma();
+
+
+        }
+    };
+
+        public void addComma(){
+            etResultCalc= findViewById(R.id.etResultCalc);
+
+            int existComma=0;
+            existComma=resultText.indexOf(',');
+            //Toast.makeText(this, "IndexOf: "+String.valueOf(existComma), Toast.LENGTH_SHORT).show();
+            if(resultText.indexOf(',')==-1) {
+                resultText = resultText + ',';
+                etResultCalc.setText(resultText);
+            }
+        }
 
     public double calculate(double secondFactor){
         double result=0.0;
@@ -158,11 +197,12 @@ public class MainActivity extends AppCompatActivity {
 
     String getStringFormat(double res)
     {
+        res=Math.round(res*100.0)/100.0;
         String strValue=String.valueOf(res);
 
         String [] strArray= strValue.split("\\.");
 
-        Toast.makeText(this, strArray[0], Toast.LENGTH_LONG).show();
+        //Toast.makeText(this, strArray[0], Toast.LENGTH_LONG).show();
 
         if(Integer.valueOf(strArray[1])==0)
              return strArray[0];
@@ -171,4 +211,8 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    String getDoubleFormat(String strInput)
+    {  return strInput.replace(',','.');
+
+    }
 }
